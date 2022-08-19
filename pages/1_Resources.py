688936f516236580c87f2c84b29a42d0fc0ec8c3
Zach Wendland <98436507+zmwendland@users.noreply.github.com>
2022-08-19 15:49:43 -0500

    Update 1_Resources.py

--- pages/1_Resources.py
+++ pages/1_Resources.py
@@ -1,1 +1,28 @@
+import streamlit as st
+import Home
+
+st.set_page_config(page_title="Resources", page_icon="📈")
+
+# st.title('Stock Market Resources')
+
+st.sidebar.header("Resources")
+st.markdown(
+    """
+    ### Financial Data & News 
+    - [Yahoo Finance](https://finance.yahoo.com)
+    - [Finviz Stock Screener](https://finviz.com)
+    - [Weekly Data and Market Calendar](https://www.marketwatch.com/economy-politics/calendar)
+    - [CNBC](https://cnbc.com)
+    - [Seeking Alpha](https://seekingalpha.com)
+    - [Zero Hedge](https://zerohedge.com)
+    - [Earnings Calendar](https://www.zacks.com/earnings/earnings-calendar)
+    - [Investing.com](https://investing.com)
+    - [Bloomberg](https://bloomberg.com)
+    - [Macroeconomic Data](https://fred.stlouisfed.org/)
+    """
+    )
+
+    
+
+
 
