0e6fc946c4fc5dd5638df7f4818a4b11c9449f1d
Zach Wendland <98436507+zmwendland@users.noreply.github.com>
2022-08-25 08:16:22 -0500

    Update 4_Economic Calendar.py

--- pages/4_Economic Calendar.py
+++ pages/4_Economic Calendar.py
@@ -32,14 +32,35 @@ df = df.loc[df['zone'].isin(['United States','Germany','Canada','Japan','Taiwan'
 df = df.sort_values(by=['zone','date'])
 
 choice = st.radio('Choose an Info Type',
-         ('All','Specific Country'))
+         ('All','USA','Specific Country'))
 
 
 if (choice=='All'):
     st.write(df)
-    
+elif (choice=='USA'):
+    countries = [x.title() for x in countries]
+    countries.sort()    
+    box = st.selectbox('Choose Country',options=countries)
+    countryForm = st.form('Choose Country')
+    submit = countryForm.form_submit_button("Go")
+    if submit:
+        start = dt.now()
+        start2 = start.strftime('%d/%m/%Y')
+        end = dt.now() + timedelta(days=14)
+        end2 = end.strftime('%d/%m/%Y')
+        
+        data = ip.economic_calendar(from_date=str(start2),to_date=str(end2))
+        
+        df = pd.DataFrame(data)
+        df = df.drop(columns=['id'])
+        df['zone'] = [x.title() for x in df['zone']]
+        df = df.loc[df['zone'].isin(['United States'])]
+        df['date'] = [x.strftime('%m/%d/%Y') for x in df['date']]
+        df = df.loc[df['zone'].isin([box])]
+        df = df.sort_values(by=['zone','date'])
+        st.write(df)
+
 else:
-    st.subheader('Under Construction')
     countries = [x.title() for x in countries]
     countries.sort()    
     box = st.selectbox('Choose Country',options=countries)
@@ -57,6 +78,7 @@ else:
         df = pd.DataFrame(data)
         df = df.drop(columns=['id'])
         df['zone'] = [x.title() for x in df['zone']]
+        df['date'] = [x.strftime('%m/%d/%Y') for x in df['date']]
         df = df.loc[df['zone'].isin([box])]
         df = df.sort_values(by=['zone','date'])
         
