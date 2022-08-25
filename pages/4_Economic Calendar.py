5283d08ef9d6c18efb917e178d4b61c72f52c24b
Zach Wendland <98436507+zmwendland@users.noreply.github.com>
2022-08-25 06:13:31 -0500

    Update 4_Economic Calendar.py

--- pages/4_Economic Calendar.py
+++ pages/4_Economic Calendar.py
@@ -14,6 +14,9 @@ import streamlit as st
 st.subheader('Economic Calendar')
 st.markdown('Dates are dd/mm/yyyy.')
 
+countries = list(['United States','Germany','Canada','Japan','Taiwan',
+                  'Euro Zone','United Kingdom','France','South Korea'])
+
 start = dt.now()
 start2 = start.strftime('%d/%m/%Y')
 end = dt.now() + timedelta(days=14)
@@ -37,15 +40,28 @@ if (choice=='All'):
     
 else:
     st.subheader('Under Construction')
-    countries = list(df.zone.unique())
     countries = [x.title() for x in countries]
     countries.sort()    
     box = st.selectbox('Choose Country',options=countries)
     countryForm = st.form('Choose Country')
     submit = countryForm.form_submit_button("Go")
     if submit:
+        
+        start = dt.now()
+        start2 = start.strftime('%d/%m/%Y')
+        end = dt.now() + timedelta(days=14)
+        end2 = end.strftime('%d/%m/%Y')
+        
+        data = ip.economic_calendar(from_date=str(start2),to_date=str(end2))
+        
+        df = pd.DataFrame(data)
+        df = df.drop(columns=['id'])
+        df['zone'] = [x.title() for x in df['zone']]
+        df = df.loc[df['zone'].isin(['United States','Germany','Canada','Japan','Taiwan',
+                                      'Euro Zone','United Kingdom','France','South Korea'])]
+        df = df.sort_values(by=['zone','date'])
         country = str(box)
         df = df.loc(df['zone'].isin(country))
-        st.dateframe(df)
+        st.write(df)
 
 
