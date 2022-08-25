b2397b5be05c99ee50640598011b12e2cda4b4d1
Zach Wendland <98436507+zmwendland@users.noreply.github.com>
2022-08-25 08:19:42 -0500

    Update 4_Economic Calendar.py

--- pages/4_Economic Calendar.py
+++ pages/4_Economic Calendar.py
@@ -39,23 +39,22 @@ if (choice=='All'):
     st.write(df)
 elif (choice=='USA'):
     countries = [x.title() for x in countries]
-    countries.sort()    
-    if submit:
-        start = dt.now()
-        start2 = start.strftime('%d/%m/%Y')
-        end = dt.now() + timedelta(days=14)
-        end2 = end.strftime('%d/%m/%Y')
-        
-        data = ip.economic_calendar(from_date=str(start2),to_date=str(end2))
-        
-        df = pd.DataFrame(data)
-        df = df.drop(columns=['id'])
-        df['zone'] = [x.title() for x in df['zone']]
-        df = df.loc[df['zone'].isin(['United States'])]
-        df['date'] = [x.strftime('%m/%d/%Y') for x in df['date']]
-        df = df.loc[df['zone'].isin([box])]
-        df = df.sort_values(by=['zone','date'])
-        st.write(df)
+    countries.sort() 
+    start = dt.now()
+    start2 = start.strftime('%d/%m/%Y')
+    end = dt.now() + timedelta(days=14)
+    end2 = end.strftime('%d/%m/%Y')
+    
+    data = ip.economic_calendar(from_date=str(start2),to_date=str(end2))
+    
+    df = pd.DataFrame(data)
+    df = df.drop(columns=['id'])
+    df['zone'] = [x.title() for x in df['zone']]
+    df = df.loc[df['zone'].isin(['United States'])]
+    df['date'] = [x.strftime('%m/%d/%Y') for x in df['date']]
+    df = df.loc[df['zone'].isin([box])]
+    df = df.sort_values(by=['zone','date'])
+    st.write(df)
 
 else:
     countries = [x.title() for x in countries]
