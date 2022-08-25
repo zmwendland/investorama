d4d94c4db1f6884ebfafd26899389c1580b88f25
Zach Wendland <98436507+zmwendland@users.noreply.github.com>
2022-08-24 23:44:30 -0500

    Update 3_Correlation Matrix.py

--- pages/3_Correlation Matrix.py
+++ pages/3_Correlation Matrix.py
@@ -68,10 +68,14 @@ if submit_button:
     
     fig, ax = plt.subplots()
     st.title('Correlation 2021-Present')
-    sns.heatmap(corr_df, cmap='RdYlGn', annot=True,vmax=1.0, vmin=-1.0 , mask = mask, linewidths=2.5,ax=ax)
+    sns.heatmap(corr_df, cmap='RdYlGn', annot=True, fmt='.2f',vmax=1.0, vmin=-1.0 , mask = mask, linewidths=2.5,ax=ax)
     # plt.yticks(rotation=0) 
     # plt.xticks(rotation=90)
     
     st.write(fig)
 else: 
     st.subheader('Correlate!')
+
+
+
+
