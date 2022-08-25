6fc3069b12ca57eeaf086c035deae525ce32236a
Zach Wendland <98436507+zmwendland@users.noreply.github.com>
2022-08-24 23:04:16 -0500

    Update 3_Correlation Matrix.py

--- pages/3_Correlation Matrix.py
+++ pages/3_Correlation Matrix.py
@@ -13,27 +13,27 @@ import seaborn
 import streamlit as st
 import matplotlib.pyplot as plt
 
-start = datetime(2021,1,1)
-stocks = [stonk,'SPY','QQQ','^RUT','GC=F','CL=F','^TNX', 'DX=F',
-          'BTC-USD','EURUSD=X','USDJPY=X']
-stocks_data = []
-s2_price = []
+# start = datetime(2021,1,1)
+# stocks = [stonk,'SPY','QQQ','^RUT','GC=F','CL=F','^TNX', 'DX=F',
+#           'BTC-USD','EURUSD=X','USDJPY=X']
+# stocks_data = []
+# s2_price = []
 
-for stock in stocks:
-    r = web.DataReader(stock,'yahoo',start)
-    r['stock'] = stock
-    stocks_data.append(r)
+# for stock in stocks:
+#     r = web.DataReader(stock,'yahoo',start)
+#     r['stock'] = stock
+#     stocks_data.append(r)
     
-df = pd.concat(stocks_data)
-df = df.reset_index()
-df = df[['Date','stock','Close']]
+# df = pd.concat(stocks_data)
+# df = df.reset_index()
+# df = df[['Date','stock','Close']]
 
-df_piv = df.pivot('Date','stock','Close').reset_index()
-corr_df = df_piv.corr(method='pearson')
-corr_df.head().reset_index()
+# df_piv = df.pivot('Date','stock','Close').reset_index()
+# corr_df = df_piv.corr(method='pearson')
+# corr_df.head().reset_index()
 
-mask = np.zeros_like(corr_df)
-mask[np.triu_indices_from(mask)] = True
+# mask = np.zeros_like(corr_df)
+# mask[np.triu_indices_from(mask)] = True
 
 def heatmaps():
     fig = seaborn.heatmap(corr_df, cmap='RdYlGn', vmax=1.0, vmin=-1.0 , mask = mask, linewidths=2.5)
@@ -71,7 +71,4 @@ if submit_button:
     st.write(heatmaps())
 else: 
     st.subheader('Correlate!')
-
-
-
 
