5d32d883007c5ab44fdddecb0a3628a437887ac0
Zach Wendland <98436507+zmwendland@users.noreply.github.com>
2022-08-25 03:41:23 -0500

    Update 5_Economic Calendar.py

--- pages/5_Economic Calendar.py
+++ pages/5_Economic Calendar.py
@@ -5,7 +5,7 @@ Created on Thu Aug 25 02:50:02 2022
 @author: Zach
 """
 
-import investpy
+import investpy as ip
 from datetime import timedelta
 from datetime import datetime as dt
 import pandas as pd
@@ -16,7 +16,7 @@ start = start.strftime('%d/%m/%Y')
 end = dt.now() + timedelta(days=365)
 end = end.strftime('%d/%m/%Y')
 
-data = investpy.economic_calendar(from_date=str(start),to_date=str(end))
+data = ip.economic_calendar(from_date=str(start),to_date=str(end))
 
 df = pd.DataFrame(data)
 
@@ -24,5 +24,6 @@ df = pd.DataFrame(data)
 countries = list(df.zone.unique())
 countries = [x.title() for x in countries]
 
-st.selectbox('Choose Country',options=countries)
-st.write(df.head(14))
+box = st.selectbox('Choose Country',options=countries)
+if box:
+    st.write(df.head(14))
