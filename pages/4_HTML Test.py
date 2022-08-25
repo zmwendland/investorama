8459581b1e39ece5aaece56113e440d67dcbcdad
Zach Wendland <98436507+zmwendland@users.noreply.github.com>
2022-08-25 02:37:05 -0500

    Update 4_HTML Test.py

--- pages/4_HTML Test.py
+++ pages/4_HTML Test.py
@@ -5,4 +5,8 @@ Created on Thu Aug 25 02:22:10 2022
 @author: Zach
 """
 import streamlit as st
+import streamlit.components.v1 as components
 st.subheader('TESTING')
+components.html("""
+<img src='https://www.marketplace.org/wp-content/uploads/2019/09/stockmarket.jpg?fit=2880%2C1621' width=100% height = 300>
+""")
