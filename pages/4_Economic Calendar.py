2a449a03dab99cb5033cf07cf4747ec85264c3ad
Zach Wendland <98436507+zmwendland@users.noreply.github.com>
2022-08-25 06:01:59 -0500

    Update 4_Economic Calendar.py

--- pages/4_Economic Calendar.py
+++ pages/4_Economic Calendar.py
@@ -11,22 +11,30 @@ from datetime import datetime as dt
 import pandas as pd
 import streamlit as st
 
-start = dt.now()
-start = start.strftime('%d/%m/%Y')
-end = dt.now() + timedelta(days=14)
-end = end.strftime('%d/%m/%Y')
+st.subheader('Economic Calendar')
+st.markdown('Dates are dd/mm/yyyy.')
 
-data = ip.economic_calendar(from_date=str(start),to_date=str(end))
+start = dt.now()
+start2 = start.strftime('%d/%m/%Y')
+end = dt.now() + timedelta(days=14)
+end2 = end.strftime('%d/%m/%Y')
+
+data = ip.economic_calendar(from_date=str(start2),to_date=str(end2))
 
 df = pd.DataFrame(data)
 df = df.drop(columns=['id'])
 df['zone'] = [x.title() for x in df['zone']]
+df = df.loc[df['zone'].isin(['United States','Germany','Canada','Japan','Taiwan',
+                              'Euro Zone','United Kingdom','France','South Korea'])]
+df = df.sort_values(by=['zone','date'])
 
 choice = st.radio('Choose an Info Type',
          ('All','Specific Country'))
 
+
 if (choice=='All'):
-    st.dataframe(df)
+    st.write(df)
+    
 else:
     st.subheader('Under Construction')
     countries = list(df.zone.unique())
@@ -35,3 +43,9 @@ else:
     box = st.selectbox('Choose Country',options=countries)
     countryForm = st.form('Choose Country')
     submit = countryForm.form_submit_button("Go")
+    if submit:
+        country = str(box)
+        df = df.loc(df['zone'].isin(country))
+        
+
+
