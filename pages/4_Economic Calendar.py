ae15d0235f10f83690476b9127ed4084d1c15df0
Zach Wendland <98436507+zmwendland@users.noreply.github.com>
2022-08-25 09:43:02 -0500

    Update 4_Economic Calendar.py

--- pages/4_Economic Calendar.py
+++ pages/4_Economic Calendar.py
@@ -11,11 +11,8 @@ from datetime import datetime as dt
 import pandas as pd
 import streamlit as st
 
-st.subheader('Economic Calendar')
+st.subheader('USA Economic Calendar')
 st.markdown('Dates are dd/mm/yyyy.')
-
-countries = list(['United States','Germany','Canada','Japan','Taiwan',
-                  'Euro Zone','United Kingdom','France','South Korea'])
 
 start = dt.now()
 start2 = start.strftime('%d/%m/%Y')
@@ -27,55 +24,9 @@ data = ip.economic_calendar(from_date=start2,to_date=end2)
 df = pd.DataFrame(data)
 df = df.drop(columns=['id'])
 df['zone'] = [x.title() for x in df['zone']]
-df = df.loc[df['zone'].isin(['United States','Germany','Canada','Japan','Taiwan',
-                              'Euro Zone','United Kingdom','France','South Korea'])]
+df['date'] = end.strftime('%m-%d-%Y')
+df = df.loc[df['zone']=='United States']
 df = df.sort_values(by=['zone','date'])
-
-choice = st.radio('Choose an Info Type',
-         ('All','USA','Specific Country'))
-
-
-if (choice=='All'):
-    st.write(df)
-elif (choice=='USA'):
-    start = dt.now()
-    start2 = start.strftime('%d/%m/%Y')
-    end = dt.now() + timedelta(days=14)
-    end2 = end.strftime('%d/%m/%Y')
-    
-    data = ip.economic_calendar(from_date=str(start2),to_date=str(end2))
-    
-    df = pd.DataFrame(data)
-    df = df.drop(columns=['id'])
-    df['date'] = pd.to_datetime(df.date)
-    df['date'] = df['date'].dt.strftime('%m-%d-%Y')
-    df['zone'] = [x.title() for x in df['zone']]
-    df = df.loc[df['zone'].isin(['United States'])]
-    df = df.sort_values(by=['zone','date'])
-    st.write(df)
-
-else:
-    countries = [x.title() for x in countries]
-    countries.sort()    
-    box = st.selectbox('Choose Country',options=countries)
-    countryForm = st.form('Choose Country')
-    submit = countryForm.form_submit_button("Go")
-    if submit:
-        
-        start = dt.now()
-        start2 = start.strftime('%d/%m/%Y')
-        end = dt.now() + timedelta(days=14)
-        end2 = end.strftime('%d/%m/%Y')
-        
-        data = ip.economic_calendar(from_date=str(start2),to_date=str(end2))
-        
-        df = pd.DataFrame(data)
-        df = df.drop(columns=['id'])
-        df['zone'] = [x.title() for x in df['zone']]
-        # df['date'] = [x.strftime('%m/%d/%Y') for x in df['date']]
-        df = df.loc[df['zone'].isin([box])]
-        df = df.sort_values(by=['zone','date'])
-        
-        st.write(df)
+st.write(df)
 
 
