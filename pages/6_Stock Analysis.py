d670b88ffeb396935612bdf3b5d10f2d9897cc38
Zach Wendland <98436507+zmwendland@users.noreply.github.com>
2022-08-31 17:06:34 -0500

    Create 6_Stock Analysis.py

---
+++ pages/6_Stock Analysis.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug 31 16:52:16 2022
+
+@author: Zach
+"""
+
+import pandas as pd
+import yfinance as yf
+import datetime as dt
+from datetime import  date, timedelta,time
+from pandas_datareader import data as pdr
+import matplotlib as mpl
+from yahoo_fin import stock_info as si
+import streamlit as st 
+
+ticker = ''
+    
+yesterday = date.today() - timedelta(days=1)
+yesterday.strftime('%m%d%y')
+
+
+start = dt.datetime.now()
+
+
+st.subheader('Single Stock Daily Performance')
+stonkForm = st.form("Enter Ticker")
+inputTicker = stonkForm.text_input('Ticker Symbol',value=ticker)
+submit_button = stonkForm.form_submit_button("GO")
+if submit_button:
+    name = stock_info.get_data(inputTicker)
+    name = list(name['ticker'])
+    name = name[0]
+    stock = stock_info.get_live_price(inputTicker)
+    stock = round(stock,2)
+    last = stock_info.get_data(inputTicker,start_date=start)
+    last = list(last['open'])
+    change = stock/round(last[0],2)-1
+elif IndexError():
+    st.subheader('Not a valid ticker')
+else:
+    st.subheader('Press GO')
