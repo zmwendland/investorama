413a5bc2aa4755cc3eefb4a2b4c5db379f794fd5
Zach Wendland <98436507+zmwendland@users.noreply.github.com>
2022-08-31 18:56:22 -0500

    Update 5_Stock Analysis.py

--- pages/5_Stock Analysis.py
+++ pages/5_Stock Analysis.py
@@ -5,30 +5,25 @@ Created on Wed Aug 31 16:52:16 2022
 @author: Zach
 """
 
-import pandas as pd
-import yfinance as yf
+# import pandas as pd
+# import yfinance as yf
 import datetime as dt
-from datetime import  date, timedelta,time
-from pandas_datareader import data as pdr
-import matplotlib as mpl
-from yahoo_fin import stock_info as si
-import streamlit as st 
+from datetime import  date, timedelta
+# import matplotlib as mpl
+from yahoo_fin import stock_info
+import streamlit as st
 
 ticker = ''
     
 yesterday = date.today() - timedelta(days=1)
 yesterday.strftime('%m%d%y')
 
-
-start = dt.datetime.now()
-
-
 st.subheader('Single Stock Daily Performance')
 stonkForm = st.form("Enter Ticker")
 inputTicker = stonkForm.text_input('Ticker Symbol',value=ticker)
 submit_button = stonkForm.form_submit_button("GO")
 if submit_button:
-    ticker = ticker
+    start = dt.datetime.now()
     name = stock_info.get_data(ticker)
     name = list(name['ticker'])
     name = name[0]
@@ -37,6 +32,6 @@ if submit_button:
     last = stock_info.get_data(ticker,start_date=start)
     last = list(last['open'])
     change = stock/round(last[0],2)-1
-
+    st.metric(label=name,value=stock,delta=change)
 else:
     st.subheader('Press GO')
